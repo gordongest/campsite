fcc3d9fdef05fd439af5b1f99b24e1c8c885589f
gordongest <gordon.gest@gmail.com>
2021-05-01 19:12:17 -0600

    commiting from root dir this time...

--- src/index.ts
+++ src/index.ts
@@ -4,21 +4,32 @@ import boxen from 'boxen';
 import clear from 'clear';
 import chalk, { bold } from 'chalk';
 import figlet from 'figlet';
-import getSites from './getSites'
+import getSites from './getSites';
 
-clear();
-
-console.log(
-  chalk.green(figlet.textSync('CampSite', { horizontalLayout: 'full' }))
-);
+const options = {
+  padding: 1,
+  margin: 1,
+  borderStyle: 'round',
+};
 
 const run = async (): Promise<void> => {
   try {
     getSites();
-  } catch(err) {
-    console.log('Sorry, I ran into a problem. Have a look at this:');
-    console.log(err.message)
+  } catch (err) {
+    console.log('Sorry, I ran into a problem. Have a look here:');
+    console.log(err.message);
   }
-}
+};
 
-boxen(run(), { padding: 1, borderStyle: 'bold', borderColor: 'cyan' });
+clear();
+
+console.log(
+  chalk.yellow(
+    boxen(
+      chalk.green(figlet.textSync('CampSite', { horizontalLayout: 'full' })),
+      options
+    )
+  )
+);
+
+run();
