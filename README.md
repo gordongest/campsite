37f6a944b5bced7b7ed59bf2fff93e28fb2c253b
gordongest <gordon.gest@gmail.com>
2021-05-02 13:05:06 -0600

    continuing to add to README, added describe block for class in test

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # CampSite
 
-**CampSite** is a Command Line Interface application that parses a local JSON data file and returns the available campsites for the specified dates.
+**CampSite** is a Command Line Interface application that parses a local JSON data file and returns the available campsites for the dates specified within.
 
 ## Running CampSite
 
@@ -12,10 +12,28 @@
 
 At the highest level, CampSite uses the following approach:
 
-- Look through the campsites
-- Associate each campsite with its reservations
-- Compare the given search dates to existing reservations and check whether the dates overlap or do not leave a sufficient gap
-- Return campsites that are either not reserved or which don't have any conflicts
+- Take in the submitted data.
+- Look through the campsites.
+- Associate each campsite with its reservations, if they exist.
+- Check for unreserved campsites.
+- Compare the given search dates to any existing reservations and check whether the dates overlap or do not leave a sufficient gap.
+- Return the campsites which are not reserved and/or do not present any conflicts.
 
-In building the core function, I chose to take a largely functional approach using implicit data return and higher-order functions when possible.
+In building the core function, I chose to take a largely functional approach using higher-order functions when possible to avoid mutation of data as well as achieve reduced space complexity and simplify testing.
 
+### Assumptions and considerations
+
+While considering the task, I made several assumptions:
+- The dates specified in the provided JSON file refer to nights, rather than days. This is evidenced by a reservation that both begins *and* ends on June 1, 2018.
+- Because of that, searching for dates that are immediately adjacent to existing reservations is acceptable.
+- There is no restriction on the minimum length of a reservation.
+- Potential visitors may have an open-ended itinerary; they may wish to search for only a start date **or** only an end date, or no specific dates at all.
+- Therefore, the user should only be prevented from viewing campsites that definitively could *not* accommodate their itinerary. In these cases, the application could be structured as to present the user with a calendar of available dates for the site they choose to view.
+
+### Extensibility
+
+Although this program defaults to check for single-night gaps, I have also specified an optional argument to allow for other minimum gaps. This should allow for some extensibility with additional development, and receives several tests to check for basic functionality.
+
+### Testing
+
+While testing the program, I strove to achieve thorough coverage using both unit and integration tests. As the bulk of the program's logic occurs in the `conflicts` method, it accordingly receives the greatest amount of testing. After ensuring that each inner method is returning its expected values, the outer method receives several integration tests.
--- test/reservations.test.js
+++ test/reservations.test.js
@@ -30,141 +30,143 @@ const mockReservations = (data, index) => {
   ];
 };
 
-describe('helper functions', () => {
-  describe('date parser', () => {
-    it('returns an object', () => {
-      const actualSearch = mockSearch('2021-04-30', '2021-05-02');
+describe('Site Checker', () => {
+  describe('inner methods', () => {
+    describe('date parser', () => {
+      it('returns an object', () => {
+        const actualSearch = mockSearch('2021-04-30', '2021-05-02');
 
-      expect(parseDateStrings(actualSearch)).toBeInstanceOf(Object);
+        expect(parseDateStrings(actualSearch)).toBeInstanceOf(Object);
+      });
+
+      it('returns an object with the desired keys', () => {
+        const actualSearch = mockSearch('2021-04-30', '2021-05-02');
+
+        expect(parseDateStrings(actualSearch)).toHaveProperty('startDate');
+        expect(parseDateStrings(actualSearch)).toHaveProperty('endDate');
+      });
+
+      it('parses date strings correctly', () => {
+        const actualSearch = mockSearch('2021-04-30', '2021-05-02');
+
+        expect(parseDateStrings(actualSearch).startDate).toBeInstanceOf(moment);
+        expect(parseDateStrings(actualSearch).endDate).toBeInstanceOf(moment);
+      });
     });
 
-    it('returns an object with the desired keys', () => {
-      const actualSearch = mockSearch('2021-04-30', '2021-05-02');
+    describe('conflict checker', () => {
+      it('identifies an overlap conflict at search start', () => {
+        const actualSearch = mockSearch('2021-04-30', '2021-05-02');
+        const actualReservations = mockReservations(testData.reservations, 0);
 
-      expect(parseDateStrings(actualSearch)).toHaveProperty('startDate');
-      expect(parseDateStrings(actualSearch)).toHaveProperty('endDate');
-    });
+        expect(conflicts(actualReservations, actualSearch, 1)).toEqual(true);
+      });
 
-    it('parses date strings correctly', () => {
-      const actualSearch = mockSearch('2021-04-30', '2021-05-02');
+      it('identifies an overlap conflict at search end', () => {
+        const actualSearch = mockSearch('2021-05-04', '2021-05-07');
+        const actualReservations = mockReservations(testData.reservations, 1);
 
-      expect(parseDateStrings(actualSearch).startDate).toBeInstanceOf(moment);
-      expect(parseDateStrings(actualSearch).endDate).toBeInstanceOf(moment);
+        expect(conflicts(actualReservations, actualSearch, 1)).toEqual(true);
+      });
+
+      it('identifies an overlap conflict at both sides', () => {
+        const actualSearch = mockSearch('2021-04-30', '2021-05-07');
+        const actualReservations = testData.reservations;
+
+        expect(conflicts(actualReservations, actualSearch, 1)).toEqual(true);
+      });
+
+      it('identifies a gap conflict at search start', () => {
+        const actualSearch = mockSearch('2021-05-03', '2021-05-05');
+        const actualReservations = mockReservations(testData.reservations, 0);
+
+        expect(conflicts(actualReservations, actualSearch, 1)).toEqual(true);
+      });
+
+      it('identifies a gap conflict at search end', () => {
+        const actualSearch = mockSearch('2021-05-02', '2021-05-04');
+        const actualReservations = mockReservations(testData.reservations, 1);
+
+        expect(conflicts(actualReservations, actualSearch, 1)).toEqual(true);
+      });
+
+      it('identifies a gap conflict at both sides', () => {
+        const actualSearch = mockSearch('2021-05-03', '2021-05-04');
+        const actualReservations = testData.reservations;
+
+        expect(conflicts(actualReservations, actualSearch, 1)).toEqual(true);
+      });
+
+      it('identifies a gap conflict of 3 nights prior', () => {
+        const actualSearch = mockSearch('2021-04-22', '2021-04-24');
+        const actualReservations = mockReservations(testData.reservations, 0);
+
+        expect(conflicts(actualReservations, actualSearch, 3)).toEqual(true);
+      });
+
+      it('identifies a gap conflict of 2 nights following', () => {
+        const actualSearch = mockSearch('2021-05-13', '2021-05-15');
+        const actualReservations = mockReservations(testData.reservations, 1);
+
+        expect(conflicts(actualReservations, actualSearch, 2)).toEqual(true);
+      });
+
+      it('identifies a non-conflicting following-day query', () => {
+        const actualSearch = mockSearch('2021-05-02', '2021-05-04');
+        const actualReservations = mockReservations(testData.reservations, 0);
+
+        expect(conflicts(actualReservations, actualSearch, 1)).toEqual(false);
+      });
+
+      it('identifies a non-conflicting prior-day query', () => {
+        const actualSearch = mockSearch('2021-05-02', '2021-05-05');
+        const actualReservations = mockReservations(testData.reservations, 1);
+
+        expect(conflicts(actualReservations, actualSearch, 1)).toEqual(false);
+      });
+
+      it('identifies a non-conflicting gap query', () => {
+        const actualSearch = mockSearch('2021-05-05', '2021-05-06');
+        const actualReservations = mockReservations(testData.reservations, 0);
+
+        expect(conflicts(actualReservations, actualSearch, 2)).toEqual(false);
+      });
     });
   });
 
-  describe('conflict checker', () => {
-    it('identifies an overlap conflict at search start', () => {
-      const actualSearch = mockSearch('2021-04-30', '2021-05-02');
-      const actualReservations = mockReservations(testData.reservations, 0);
+  describe('primary method', () => {
+    it('returns an array', () => {
+      const actual = mockData(testData, null, null);
 
-      expect(conflicts(actualReservations, actualSearch, 1)).toEqual(true);
+      expect(availableSites(actual, 1)).toBeInstanceOf(Array);
     });
 
-    it('identifies an overlap conflict at search end', () => {
-      const actualSearch = mockSearch('2021-05-04', '2021-05-07');
-      const actualReservations = mockReservations(testData.reservations, 1);
+    it('returns all sites when no search dates are provided', () => {
+      const actual = mockData(testData, null, null);
 
-      expect(conflicts(actualReservations, actualSearch, 1)).toEqual(true);
+      expect(availableSites(actual, 1)).toEqual([
+        'Mountain Lodge',
+        'Log Cabin',
+        'Old Shack',
+      ]);
     });
 
-    it('identifies an overlap conflict at both sides', () => {
-      const actualSearch = mockSearch('2021-04-30', '2021-05-07');
-      const actualReservations = testData.reservations;
+    it('returns compatible sites with only start date provided', () => {
+      const actual = mockData(testData, '2021-05-02', null);
 
-      expect(conflicts(actualReservations, actualSearch, 1)).toEqual(true);
+      expect(availableSites(actual, 1)).toEqual(['Mountain Lodge', 'Log Cabin']);
     });
 
-    it('identifies a gap conflict at search start', () => {
-      const actualSearch = mockSearch('2021-05-03', '2021-05-05');
-      const actualReservations = mockReservations(testData.reservations, 0);
+    it('returns compatible sites with only end date provided', () => {
+      const actual = mockData(testData, null, '2021-04-28');
 
-      expect(conflicts(actualReservations, actualSearch, 1)).toEqual(true);
+      expect(availableSites(actual, 1)).toEqual(['Log Cabin', 'Old Shack']);
     });
 
-    it('identifies a gap conflict at search end', () => {
-      const actualSearch = mockSearch('2021-05-02', '2021-05-04');
-      const actualReservations = mockReservations(testData.reservations, 1);
+    it('returns compatible sites both dates provided', () => {
+      const actual = mockData(testData, '2021-05-04', '2021-05-07');
 
-      expect(conflicts(actualReservations, actualSearch, 1)).toEqual(true);
+      expect(availableSites(actual, 1)).toEqual(['Mountain Lodge', 'Old Shack']);
     });
-
-    it('identifies a gap conflict at both sides', () => {
-      const actualSearch = mockSearch('2021-05-03', '2021-05-04');
-      const actualReservations = testData.reservations;
-
-      expect(conflicts(actualReservations, actualSearch, 1)).toEqual(true);
-    });
-
-    it('identifies a gap conflict of 3 nights prior', () => {
-      const actualSearch = mockSearch('2021-04-22', '2021-04-24');
-      const actualReservations = mockReservations(testData.reservations, 0);
-
-      expect(conflicts(actualReservations, actualSearch, 3)).toEqual(true);
-    });
-
-    it('identifies a gap conflict of 2 nights following', () => {
-      const actualSearch = mockSearch('2021-05-13', '2021-05-15');
-      const actualReservations = mockReservations(testData.reservations, 1);
-
-      expect(conflicts(actualReservations, actualSearch, 2)).toEqual(true);
-    });
-
-    it('identifies a non-conflicting following-day query', () => {
-      const actualSearch = mockSearch('2021-05-02', '2021-05-04');
-      const actualReservations = mockReservations(testData.reservations, 0);
-
-      expect(conflicts(actualReservations, actualSearch, 1)).toEqual(false);
-    });
-
-    it('identifies a non-conflicting prior-day query', () => {
-      const actualSearch = mockSearch('2021-05-02', '2021-05-05');
-      const actualReservations = mockReservations(testData.reservations, 1);
-
-      expect(conflicts(actualReservations, actualSearch, 1)).toEqual(false);
-    });
-
-    it('identifies a non-conflicting gap query', () => {
-      const actualSearch = mockSearch('2021-05-05', '2021-05-06');
-      const actualReservations = mockReservations(testData.reservations, 0);
-
-      expect(conflicts(actualReservations, actualSearch, 2)).toEqual(false);
-    });
-  });
-});
-
-describe('main function', () => {
-  it('returns an array', () => {
-    const actual = mockData(testData, null, null);
-
-    expect(availableSites(actual, 1)).toBeInstanceOf(Array);
-  });
-
-  it('returns all sites when no search dates are provided', () => {
-    const actual = mockData(testData, null, null);
-
-    expect(availableSites(actual, 1)).toEqual([
-      'Mountain Lodge',
-      'Log Cabin',
-      'Old Shack',
-    ]);
-  });
-
-  it('returns compatible sites with only start date provided', () => {
-    const actual = mockData(testData, '2021-05-02', null);
-
-    expect(availableSites(actual, 1)).toEqual(['Mountain Lodge', 'Log Cabin']);
-  });
-
-  it('returns compatible sites with only end date provided', () => {
-    const actual = mockData(testData, null, '2021-04-28');
-
-    expect(availableSites(actual, 1)).toEqual(['Log Cabin', 'Old Shack']);
-  });
-
-  it('returns compatible sites both dates provided', () => {
-    const actual = mockData(testData, '2021-05-04', '2021-05-07');
-
-    expect(availableSites(actual, 1)).toEqual(['Mountain Lodge', 'Old Shack']);
   });
 });
